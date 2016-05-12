049d2d6fa2a228a7513928ad02965d85659f077c
Robert Shepherd <rob.s.shepherd@gmail.com>
2016-05-12 17:55:16 +0800

    Added source model to be use groups as config option

---
+++ app/code/community/Doghouse/Carousel/Model/Config/Source/Groups.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Doghouse_Carousel_Model_Group
+ *
+ * @category  Doghouse
+ * @package   Doghouse_Carousel
+ * @author    Doghouse <support@dhmedia.com.au>
+ * @copyright 2015 Doghouse Media (http://doghouse.agency)
+ * @license   https://github.com/DoghouseMedia/Doghouse_Carousel/blob/master/LICENSE  The MIT License (MIT)
+ * @link      https://github.com/DoghouseMedia/Doghouse_Carousel
+ */
+class Doghouse_Carousel_Model_Config_Source_Groups extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
+{
+	/**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function getAllOptions()
+    {
+        $collection = Mage::getModel('dhcarousel/group')->getCollection()->toOptionArray();
+        array_unshift($collection, array('value'=> '', 'label'=> Mage::helper('adminhtml')->__('-- Please Select --')));
+        return $collection;
+    }
+}
